42d2c4a1d50f8c35b5569c5b05ed2502c5655f6e
Pushpendra <pushpendra.s@turvo.com>
2024-07-11 12:04:38 +0530

    Added starting 5 tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,70 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    return res.status(200).json({message: "Books got successfully", data: books});
+  }
+  catch(err) {
+    return res.status(500).json({message: "Internal server error !!"});
+  }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    let isbn = req["params"]["isbn"];
+    if(books[isbn]) return res.status(200).json({message: "Books got successfully", data: books[isbn]});
+    else return res.status(200).json({message: "There is no book regarding this ISBN !!", data: []});
+  }
+  catch(err) {
+    return res.status(500).json({message: "Internal server error !!"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    let author = req["params"]["author"], bookDetails;
+    for(let key in books) {
+      if(books[key]["author"] == author){
+        bookDetails = books[key]
+      }
+    }
+    if(bookDetails) return res.status(200).json({message: "Books got successfully", data: bookDetails});
+    else return res.status(200).json({message: "There is no book regarding this Author !!", data: []});
+  }
+  catch(err) {
+    return res.status(500).json({message: "Internal server error !!"});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    let title = req["params"]["title"], bookDetails;
+    for(let key in books) {
+      if(books[key]["title"] == title){
+        bookDetails = books[key]
+      }
+    }
+    if(bookDetails) return res.status(200).json({message: "Books got successfully", data: bookDetails});
+    else return res.status(200).json({message: "There is no book regarding this Author !!", data: []});
+  }
+  catch(err) {
+    return res.status(500).json({message: "Internal server error !!"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    let isbn = req["params"]["isbn"];
+    if(books[isbn]) return res.status(200).json({message: "Books got successfully", data: books[isbn]["reviews"]});
+    else return res.status(200).json({message: "There is no book regarding this ISBN !!", data: []});
+  }
+  catch(err) {
+    return res.status(500).json({message: "Internal server error !!"});
+  }
 });
 
 module.exports.general = public_users;
